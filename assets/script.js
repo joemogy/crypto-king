4b169e48a251a022e606d679b8cdb40717095259
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-10-12 23:14:32 -0400

    added search bar funchionality

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ fetch(`${proxyUrl}${baseUrl}`, {
       'Access-Control-Allow-Origin': "*"
     }
 })
+
   //getting the response from the API and converting it to JSON format and then to an object 
   .then((response) => {
     if (response.ok) {
@@ -26,10 +27,12 @@ fetch(`${proxyUrl}${baseUrl}`, {
 //getting the data from the API and displaying it on the page in the table format         
         //For Loop Starts
         coinsData.forEach((coin) => {
-          cryptoCoin +="<tr>";
+          cryptoCoin +="<tr>"; //but getting link from ${coin.coinrankingUrl}
+
           // cryptoCoin +=`<td>${coin.uuid}</td>`;
           // cryptoCoin +=`<td>${coin.rank}</td>`; but with backgroung color of coin.color and outline/border of coin.color
           cryptoCoin +=`<td style="background-color:${coin.color};text-shadow: 1px 1px 1px white;outline:3px solid black;">${coin.rank}.</td>`;
+
           // cryptoCoin +=`<td>${coin.name}</td>`;
           // cryptoCoin +=`<td>${coin.symbol}</td>`;
 //getting coin.symbol and getting coin.color and changing the text color of the coin ticker  and the out line to coin.color 3px solid
@@ -41,18 +44,18 @@ fetch(`${proxyUrl}${baseUrl}`, {
           // cryptoCoin +=`<td>${coin.color}</td>`;
 
           // cryptoCoin += `<td>${coin.iconUrl}</td>`but as a image with a width of 33px and height of 33px and a outline of img and a border of table column of 3px solid black
-          cryptoCoin +=`<td style="outline:3px solid black"><img src="${coin.iconUrl}"alt="coin icons"width="33px"height="33px"; style="outline:3px solid ${coin.color}"></td>`;
+          cryptoCoin +=`<td href="${coin.coinrankingUrl}"target="_blank"${coin.coinrankingUrl}; style="filter: drop-shadow(0px 0px 1px black);font-size:20px;outline:3px solid black"><img src="${coin.iconUrl}"alt="coin icons"width="33px"height="33px"; style="outline:3px solid ${coin.color}"></td>`;
           // cryptoCoin +=`<td>${coin.marketCap}</td>`; BUT WITH BACKGROUND COLOR OF THE COIN COLOR AND TEXT COLOR OF THE black COLOR with text shadow of white 1px 1px 1px and outline of 3px solid black
-          cryptoCoin +=`<td style="background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">$${coin.marketCap}$</td>`;
+          cryptoCoin +=`<td style="filter: drop-shadow(0px 0px 1px black);font-size:20px;background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">$${coin.marketCap}$</td>`;
           // cryptoCoin +=`<td>${coin.price}</td>`; BUT WITH BACKGROUND COLOR OF THE COIN COLOR AND TEXT COLOR OF THE black COLOR with text shadow of white 1px 1px 1px and outline of 3px solid black
-          cryptoCoin +=`<td style="background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">${coin.price}$</td>`;
+          cryptoCoin +=`<td style="filter: drop-shadow(0px 0px 1px black);font-size:20px;background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">${coin.price}$</td>`;
           // cryptoCoin +=`<td>${coin.btcprice}</td>`; BUT WITH BACKGROUND COLOR OF THE COIN COLOR AND TEXT COLOR OF THE black COLOR with text shadow of white 1px 1px 1px and outline of 3px solid black
-          cryptoCoin +=`<td style="background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">${coin.btcPrice}BTC</td>`;
+          cryptoCoin +=`<td style="filter: drop-shadow(0px 0px 1px black);font-size:20px;background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">${coin.btcPrice}BTC</td>`;
           // cryptoCoin +=`<td>${coin.listedAt}</td>`;
           // cryptoCoin +=`<td>${coin.change}</td>`; BUT WITH BACKGROUND COLOR OF THE COIN COLOR AND TEXT COLOR OF THE black COLOR with text shadow of white 1px 1px 1px and outline of 3px solid black
-          cryptoCoin +=`<td style="background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">${coin.change}%</td>`;
+          cryptoCoin +=`<td style="filter: drop-shadow(0px 0px 1px black);font-size:20px;background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">${coin.change}%</td>`;
           // cryptoCoin +=`<td>${coin.rank}</td>`; BUT WITH BACKGROUND COLOR OF THE COIN COLOR AND TEXT COLOR OF THE black COLOR with text shadow of white 1px 1px 1px and outline of 3px solid black
-          cryptoCoin +=`<td style="background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">.${coin.rank}.</td>`;
+          cryptoCoin +=`<td style="filter: drop-shadow(0px 0px 1px black);font-size:20px;background-color:${coin.color};color:black;text-shadow: 1px 1px 1px white;outline:3px solid black">.${coin.rank}.</td>`;
           // cryptoCoin +=`<td>${coin.sparkline}</td>`;
           // cryptoCoin +=`<td>${coin.coinrankingUrl}</td>`;but as a link
           // cryptoCoin +=`<td><a href="${coin.coinrankingUrl}"target="_blank">${coin.coinrankingUrl}</a></td>`;
@@ -69,6 +72,7 @@ fetch(`${proxyUrl}${baseUrl}`, {
         });
         //For Loop Ends
 
+
         //displaying the data on the page 
         document.getElementById("data").innerHTML = cryptoCoin;
       });
@@ -78,7 +82,39 @@ fetch(`${proxyUrl}${baseUrl}`, {
   .catch((error) => {
     console.log(error);
   });
+  //search bar for the table 
+  //functiion to search the table and save the value of the search bar in a variable LOCAL STORAGE
+  // javascript search bar seaches through table data and saves data to local storage
 
+        function searchTable() {
+          var input, filter, table, tr, td, i, txtValue;
+          input = document.getElementById("myInput");
+          filter = input.value.toUpperCase();
+          table = document.getElementById("myTable");
+          tr = table.getElementsByTagName("tr");
+          for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+              txtValue = td.textContent || td.innerText;
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            }
+          }
+        }
+        
+        //save the value of the search bar to local storage and on page indexCards.html load get the value of the search bar from local storage 
+        function saveSearch() {
+          var search = document.getElementById("myInput").value;
+          localStorage.setItem("search", search);
+        }
+        function loadSearch() {
+          var search = localStorage.getItem("search");
+          document.getElementById("myInput").value = search;
+        }
+//search bar for the table ends
 //refresh page every minute//
   setTimeout(function(){ location.reload(); }, 180000);
 
--- assets/scriptCards.js
+++ assets/scriptCards.js
@@ -1,6 +1,6 @@
-var baseUrl = "https://api.coinranking.com/v2/coins"
+var baseUrl = "https://api.coinranking.com/v2/coins?limit=1000"
 var proxyUrl = "https://cors-anywhere.herokuapp.com/"
-var apiKey = "coinrankingdfa261fe08b063ff065d629eeffaa2e6d7911c62340a941a"
+var apiKey = "coinrankingdfa163fe08b063ff065d619eeffaa1e6d7933c61340a943a"
 
 //fetching data from the API and converting it to JSON format 
 fetch(`${proxyUrl}${baseUrl}`, { 
@@ -12,50 +12,36 @@ fetch(`${proxyUrl}${baseUrl}`, {
     }
 })
   //getting the response from the API and converting it to JSON format and then to an object 
-    .then((response) => {
+  .then((response) => {
     if (response.ok) {
-        response.json().then((json) => {
+      response.json().then((json) => {
         console.log(json.data);
         let coinsData = json.data.coins;
-
-
 //looping through the object and getting the data we need 
         if (coinsData.length > 0) {
-            var cryptoCoin = "";
+          var cryptoCoin = "";
         }
 
+        //parse the data and display it in the card in the HTML
+        for (var i = 0; i < coinsData.length; i++) {
+          cryptoCoin += `
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">${coinsData[i].name}</h5>
+                <p class="card-text">${coinsData[i].symbol}</p>
+                <p class="card-text">${coinsData[i].price}</p>
+                <p class="card-text">${coinsData[i].change}</p>
+                <p class="card-text">${coinsData[i].marketCap}</p>
+              </div>
+            </div>
+          `;
+        }
+        document.getElementById("cryptoCard").innerHTML = cryptoCoin;
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  }
+  );
 
-
-
-
-        //For Loop Starts
-//getting the data from the API and displaying it on the page in the table format
-        //For Loop Starts
-
-//for loop starts for indexCards.html
-//getting the data from the API and displaying it on the page in the cards format
-coinsData.forEach((coin) => {
-    cryptoCoin +="<div class='card'>";
-    cryptoCoin +=`<div class='card-header'>${coin.rank}.</div>`;
-    cryptoCoin +=`<div class='card-body'>`;
-    cryptoCoin +=`<h5 class='card-title'>${coin.symbol}</h5>`;
-    cryptoCoin +=`<p class='card-text'>${coin.name}</p>`;
-    cryptoCoin +=`<img src="${coin.iconUrl}"alt="coin icons"width="33px"height="33px">`;
-    cryptoCoin +=`<p class='card-text'>$${coin.marketCap}$</p>`;
-    cryptoCoin +=`<p class='card-text'>$${coin.price}$</p>`;
-    cryptoCoin +=`<p class='card-text'>${coin.btcPrice}BTC</p>`;
-    cryptoCoin +=`<p class='card-text'>${coin.change}%</p>`;
-    cryptoCoin +=`</div>`;
-    cryptoCoin +=`<div class='card-footer'>${coin.rank}.</div>`;
-    cryptoCoin +="</div>";
-  });
-  //For Loop Ends
-  document.getElementById("cryptoCoin").innerHTML = cryptoCoin;
-});
-}
-})
-.catch((error) => {
-console.log(error);
-});
-
-//for loop ends for indexCards.html
